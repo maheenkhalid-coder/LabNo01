025969df2e7a27842f9852b6c23020a8af697e79
maheenkhalid-coder <64140831+maheenkhalid-coder@users.noreply.github.com>
2020-09-25 22:07:25 +0500

    Task Number 01 OOP(JAVA)

---
+++ src/lab01/task.java
@@ -0,0 +1,24 @@
+package lab01;
+
+public class task {
+
+	public static void main(String[] args) {
+		
+		
+		// task number 01
+		
+			System.out.println("Name: \"Maheen khalid\"\nAge: 19\n ");
+			
+		// task number 02
+			
+			System.out.println("**      **");
+			System.out.println("* *    * *");
+			System.out.println("*  *  *  *");
+			System.out.println("*    *   *");
+			System.out.println("*        *");
+			System.out.println("*        *");
+			System.out.println("*        *");
+			
+	}
+
+}
